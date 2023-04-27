bc632fcd570fcda8ed7fca89e5abd032331a8800
Matthew Eng <129238647+Mattheweng1@users.noreply.github.com>
2023-04-26 20:09:18 -0500

    Add functionality for multiple drawing tools
    Added three buttons to switch drawing tools. Drawing tools now
    change item css directly instead of adding a class to item.
    Separate createCanvas() into createCanvas() and
    promptCreateCanvas(). Minor changes to css styling.

--- script.js
+++ script.js
@@ -34,25 +34,88 @@ function deleteRowsAndItemsAll() {
     })
 }
 
-function addDrawnClass(element) {
-    element.classList.add('drawn');
+addBlackBackground = function (element) {
+    let rgbaValues = [0,0,0,1];
+    element.style.cssText = `background-color: rgba(${rgbaValues})`;
+};
+
+addRandomBackground = function (element) {
+    let rgbaValues = [0,0,0,0]
+    rgbaValues = rgbaValues.map((value) => value = Math.floor(Math.random() * 256));
+    rgbaValues[3] = 1;
+    element.style.cssText = `background-color: rgba(${rgbaValues})`;
+};
+
+reduceBackgroundOpacity = function (element) {
+    let cssString = element.style.cssText;
+    if (cssString === "") {
+        cssString = "background-color: rgba(255, 255, 255, 1);";
+    } else if (cssString.includes("background-color: rgb(")) {
+        cssString = element.style.cssText.replace("background-color: rgb(","background-color: rgba(");
+        cssString = element.style.cssText.replace(")", ", 1)");
+    }
+    const rgbaString = cssString.slice(23,-2);
+    const rgbaArray = rgbaString.split(",");
+    let rgbaArrayNew = rgbaArray;
+    rgbaArrayNew[3] = rgbaArrayNew[3] - 0.1;
+    rgbaStringNew = rgbaArrayNew.join();
+    element.style.cssText = cssString.replace(rgbaString,rgbaStringNew);
+
+    console.log(element.style.cssText);
 }
 
-function createCanvas() {
-    dimensions = prompt("How many rows and columns do you want?")
-    while (!(dimensions >= 2 && dimensions <= 100)) {
-        dimensions = prompt("Please choose a number from 2 to 100.")
-    }
+function createCanvas(dimensions) {
     deleteRowsAndItemsAll();
     addRows(dimensions);
     addItems(dimensions);
 
     const canvasItems = Array.from(document.querySelectorAll('.canvasItem'));
     canvasItems.forEach((canvasItem) => {
-        canvasItem.addEventListener('mouseover', () => addDrawnClass(canvasItem));
+        canvasItem.addEventListener('mouseover', () => addBlackBackground(canvasItem));
     })
 }
 
-const buttonCreateCanvas = document.getElementById('createCanvas');
-buttonCreateCanvas.addEventListener('click', () => {createCanvas()});
+createCanvas(100);
 
+function promptCreateCanvas() {
+    dimensions = prompt("How many rows and columns do you want?")
+    while (!(dimensions >= 2 && dimensions <= 100)) {
+        dimensions = prompt("Please choose a number from 2 to 100.");
+    }
+    createCanvas(dimensions);
+}
+
+function addHoverEventToItems(fn) {
+    let canvasItems = Array.from(document.querySelectorAll('.canvasItem'));
+    canvasItems.forEach((canvasItem) => {
+        canvasItem.replaceWith(canvasItem.cloneNode(true));
+    })
+    canvasItems = Array.from(document.querySelectorAll('.canvasItem'));
+    canvasItems.forEach((canvasItem) => {
+        canvasItem.addEventListener('mouseover', () => fn(canvasItem));
+    })
+}
+
+function useBlackPen() {
+    addHoverEventToItems(addBlackBackground);
+}
+
+function useRainbowPen() {
+    addHoverEventToItems(addRandomBackground);
+}
+
+function useSoftBrush() {
+    addHoverEventToItems(reduceBackgroundOpacity);
+}
+
+const buttonCreateCanvas = document.getElementById('createCanvas');
+buttonCreateCanvas.addEventListener('click', () => {promptCreateCanvas()});
+
+const buttonBlackPen = document.getElementById('blackPen');
+buttonBlackPen.addEventListener('click', () => useBlackPen());
+
+const buttonRainbowPen = document.getElementById('rainbowPen');
+buttonRainbowPen.addEventListener('click', () => useRainbowPen())
+
+const buttonSoftBrush = document.getElementById('softBrush');
+buttonSoftBrush.addEventListener('click', () => useSoftBrush());
